0b964a232e6e584c6048c40f1affaad60dc4a5eb
Simon Walther <simon.walt@hotmail.com>
2020-02-24 10:16:07 +0100

    add missing buttons on control panel

--- src/main/java/ch/heigvd/mcr/labo1/GUI/ControlPanelGUI.java
+++ src/main/java/ch/heigvd/mcr/labo1/GUI/ControlPanelGUI.java
@@ -8,11 +8,14 @@ import javax.swing.*;
 import java.awt.*;
 
 public class ControlPanelGUI {
+    private JButton numericViewButton = new JButton("Horloge numérique");
+    private JButton dialRomanViewButton = new JButton("Horloge romaine");
+    private JButton dialArabicViewButton = new JButton("Horloge arabe");
+    private JButton mixedClockViewButton = new JButton("Horloge mixte");
     private JButton startClockButton = new JButton("Démarrer");
     private JButton stopClockButton = new JButton("Arreter");
     private JButton resetClockButton = new JButton("Réinitialiser");
-    private JButton numericViewButton = new JButton("Horloge numérique");
-    private JButton dialRomanViewButton = new JButton("Horloge Romaine");
+    private JButton exitButton = new JButton("Quitter");
 
     private JFrame frame;
     private Clock clock;
@@ -25,20 +28,25 @@ public class ControlPanelGUI {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new GridLayout(2, 4));
 
-        startClockButton.addActionListener(actionEvent -> clock.start());
-        numericViewButton.addActionListener(actionEvent -> createNumericView(numericViewButton.getText()));
+        // Add action to buttons
         dialRomanViewButton.addActionListener(actionEvent -> createDialView(dialRomanViewButton.getText()));
-
-        stopClockButton.addActionListener(actionEvent -> clock.stop());
+        dialArabicViewButton.addActionListener(actionEvent -> createDialView(dialArabicViewButton.getText()));
+        numericViewButton.addActionListener(actionEvent -> createNumericView(numericViewButton.getText()));
+        // TODO: mixteClockViewButton action
+        startClockButton.addActionListener(actionEvent -> clock.start());
         resetClockButton.addActionListener(actionEvent -> clock.reset());
-
+        stopClockButton.addActionListener(actionEvent -> clock.stop());
+        // TODO : close app exitButton.addActionListener(actionEvent -> );
 
         // Add elements to content pane of frame
-        frame.getContentPane().add(numericViewButton);
         frame.getContentPane().add(dialRomanViewButton);
+        frame.getContentPane().add(dialArabicViewButton);
+        frame.getContentPane().add(numericViewButton);
+        frame.getContentPane().add(mixedClockViewButton);
         frame.getContentPane().add(startClockButton);
         frame.getContentPane().add(stopClockButton);
         frame.getContentPane().add(resetClockButton);
+        frame.getContentPane().add(exitButton);
 
         frame.pack();
         frame.setVisible(true);
