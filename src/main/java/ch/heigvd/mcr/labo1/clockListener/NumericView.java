4a684a050001cba7813bb4958b8a5ffaf54e0ce0
Simon Walther <simon.walt@hotmail.com>
2020-02-27 16:04:04 +0100

    fix numericView

--- src/main/java/ch/heigvd/mcr/labo1/clockListener/NumericView.java
+++ src/main/java/ch/heigvd/mcr/labo1/clockListener/NumericView.java
@@ -1,16 +1,20 @@
 package ch.heigvd.mcr.labo1.clockListener;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class NumericView extends ClockView {
+    private JLabel timeLabel;
+
     public NumericView(int width, int height) {
         super(width, height);
-
-        //setText("00:00:00");
+        timeLabel = new JLabel("00:00:00");
+        timeLabel.setFont(new Font("Serif", Font.PLAIN, 14));
+        add(timeLabel);
     }
 
     public void update(int totalSeconds){
         super.update(totalSeconds);
-        //setText(String.format("%02d:%02d:%02d", hours(), minutes(), seconds()));
+        timeLabel.setText(String.format("%02d:%02d:%02d", hours(), minutes(), seconds()));
     }
 }
